f2f76218e4ce2868336a3ec63d8d6e3ba6b0cbb3
Khursheed <khursheed.er.24@gmail.com>
2021-05-09 14:19:36 +0400

    Install dependencies via npm install axios for making network request over http enforce type safety using interfaces

---
+++ index.ts
@@ -0,0 +1,30 @@
+import axios from "axios"
+
+const url = "https://jsonplaceholder.typicode.com/todos/1";
+
+
+// declare interface to specify the Response object data type to enable strong typing
+interface Todo{
+  id:number;
+  title:string;
+  completed:boolean;
+}
+
+axios.get(url).then(response =>{
+  const todo = response.data as Todo;
+   
+  const id = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
+  
+  logTodo(id,title,completed);
+  
+});
+
+const logTodo = (id, title, completed) => {
+  console.log(`
+  The to do with Id : ${id}
+  Has a title of : ${title}
+  Is it finsihed: ${completed}
+`);
+}
